f9b2cfc414ac5817436d229d5e3b83ef53fb2f74
Teethedev@outlook.com <Teethedev@outlook.com>
2022-07-08 22:05:05 +0200

    Updated InitialSeed data

--- src/Services/catalog.api/Migrations/20220708200331_InitialSeed.Designer.cs
+++ src/Services/catalog.api/Migrations/20220708200331_InitialSeed.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using catalog.api.Services.CatalogDbContext;
 
 namespace catalog.api.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220708200331_InitialSeed")]
+    partial class InitialSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
